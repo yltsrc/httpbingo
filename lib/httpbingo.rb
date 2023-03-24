c20db06872cc565ce586d309f679418278271787
Yura  Tolstik <yury.tolstik@tripledotfortune.com>
2023-03-24 11:07:12 +0300

    init httpbino api gem

---
+++ lib/httpbingo.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "httpbingo/version"
+
+module Httpbingo
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/httpbingo_spec.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+RSpec.describe Httpbingo do
+  it "has a version number" do
+    expect(Httpbingo::VERSION).not_to be nil
+  end
+end
